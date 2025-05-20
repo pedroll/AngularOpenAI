5857b53d3cbd7461a55f22a9a4521dab816e71d1
Pedro LLongo <pedrollongo@gmail.com>
2025-05-20 17:48:29 +0200

    prepare aaudioToText

--- src/app/presentation/pages/audio-to-text-page/audio-to-text-page.component.ts
+++ src/app/presentation/pages/audio-to-text-page/audio-to-text-page.component.ts
@@ -1,12 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChatMessageComponent } from '@components/chat-bubbles/chat-message/chat-message.component';
+import { TypingLoaderComponent } from '@components/typing-loader/typing-loader.component';
+import { UserMessageComponent } from '@components/chat-bubbles/user-message/user-message.component';
+import { Message } from '@interfaces/message.interface';
+import { OpenaiService } from '../../services/openai.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  TextMessageBoxFileComponent,
+  TextMessageFileEvent,
+} from '@components/messages-boxes/text-message-box-file/text-message-box-file.component';
 
 @Component({
   selector: 'app-audio-to-text-page',
-  imports: [],
+  imports: [
+    ChatMessageComponent,
+    TypingLoaderComponent,
+    UserMessageComponent,
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    TypingLoaderComponent,
+    UserMessageComponent,
+    TextMessageBoxFileComponent,
+  ],
   templateUrl: './audio-to-text-page.component.html',
   styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AudioToTextPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openaiService = inject(OpenaiService);
 
+  handleMessageWithFile({ prompt, file }: TextMessageFileEvent): void {
+    console.log({ prompt, file });
+  }
 }
