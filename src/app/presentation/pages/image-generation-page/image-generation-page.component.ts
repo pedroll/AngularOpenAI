8a59df74aac7acf1615cfad7cbd2d8ea69b44984
Pedro LLongo <pedrollongo@gmail.com>
2025-05-29 00:23:57 +0200

    - Image generation page template

--- src/app/presentation/pages/image-generation-page/image-generation-page.component.ts
+++ src/app/presentation/pages/image-generation-page/image-generation-page.component.ts
@@ -1,12 +1,42 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import {
+  ChatMessageComponent,
+  TextMessageBoxComponent,
+  TypingLoaderComponent,
+  UserMessageComponent,
+} from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenaiService } from '../../services/openai.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-image-generation-page',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    TypingLoaderComponent,
+    UserMessageComponent,
+    TextMessageBoxComponent,
+    TypingLoaderComponent,
+  ],
   templateUrl: './image-generation-page.component.html',
   styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImageGenerationPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openaiService = inject(OpenaiService);
 
+  handleMessage(prompt: string): void {
+    console.log(prompt);
+  }
+
+  // handleMessageWithFile({ prompt, file }: TextMessageFileEvent): void {
+  //   console.log({ prompt, file });
+  // }
+  //
+  // handleMessageWithSelect({ prompt, selectedOption }: TextMessageSelectEvent): void {
+  //   console.log({ prompt, selectedOption });
+  // }
 }
