08375b6c55dc73c866da07732c85116141ff464c
Pedro LLongo <pedrollongo@gmail.com>
2025-05-30 22:51:20 +0200

    - image variation: convert img into canvas

--- src/app/presentation/components/chat-bubbles/chat-message-editable-image/chat-message-editable-image.component.ts
+++ src/app/presentation/components/chat-bubbles/chat-message-editable-image/chat-message-editable-image.component.ts
@@ -1,4 +1,14 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  signal,
+  ViewChild,
+} from '@angular/core';
 
 import { MarkdownComponent } from 'ngx-markdown';
 
@@ -11,11 +21,33 @@ import { ImageGeneratedInfo } from '@interfaces/index';
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChatMessageEditableImageComponent {
+export class ChatMessageEditableImageComponent implements AfterViewInit {
   @Input({ required: true }) text!: string;
   @Input({ required: true }) imageInfo!: ImageGeneratedInfo;
 
   @Output() selectedImage = new EventEmitter<string>();
+
+  @ViewChild('canvas', { static: false }) canvas!: ElementRef<HTMLCanvasElement>;
+
+  // eslint-disable-next-line unicorn/no-useless-undefined
+  public originalImage = signal<HTMLImageElement | undefined>(undefined);
+
+  ngAfterViewInit(): void {
+    // after canvas creted
+    if (!this.canvas.nativeElement) return;
+    const canvas = this.canvas.nativeElement;
+    const context = canvas.getContext('2d');
+
+    const image = new Image();
+    image.src = this.imageInfo.url;
+    image.crossOrigin = 'Anonymous';
+
+    this.originalImage.set(image);
+
+    image.addEventListener('load', () => {
+      context?.drawImage(image, 0, 0, canvas.width, canvas.height);
+    });
+  }
 
   handleSelectedImage() {
     this.selectedImage.emit(this.imageInfo.url);
