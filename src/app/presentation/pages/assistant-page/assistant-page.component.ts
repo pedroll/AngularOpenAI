47f844096d1abcf5305645b8875cf1bbb2624dae
Pedro LLongo <pedrollongo@gmail.com>
2025-06-01 11:33:19 +0200

    - assistant:  prepare component page

--- src/app/presentation/pages/assistant-page/assistant-page.component.ts
+++ src/app/presentation/pages/assistant-page/assistant-page.component.ts
@@ -1,12 +1,50 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import {
+  ChatMessageComponent,
+  TextMessageBoxComponent,
+  TypingLoaderComponent,
+  UserMessageComponent,
+} from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenaiService } from '../../services/openai.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-assistant-page',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    TypingLoaderComponent,
+    UserMessageComponent,
+    TextMessageBoxComponent,
+  ],
   templateUrl: './assistant-page.component.html',
-  styleUrl: './assistant-page.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AssistantPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openaiService = inject(OpenaiService);
 
+  handleMessage(prompt: string): void {
+    console.log(prompt);
+    this.isLoading.set(true);
+    this.messages.update(messages => [...messages, { text: prompt, isGpt: false }]);
+
+    this.openaiService.checkOrthography(prompt).subscribe(response => {
+      this.isLoading.set(false);
+      this.messages.update(messages => [
+        ...messages,
+        { text: response.message, isGpt: true, info: response },
+      ]);
+    });
+  }
+
+  // handleMessageWithFile({ prompt, file }: TextMessageFileEvent): void {
+  //   console.log({ prompt, file });
+  // }
+  //
+  // handleMessageWithSelect({ prompt, selectedOption }: TextMessageSelectEvent): void {
+  //   console.log({ prompt, selectedOption });
+  // }
 }
